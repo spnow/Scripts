0a7ba53d808deb9e0ae8ee3e59818443afffa421
dtctd <udtctd@gmail.com>
2016-09-28 16:53:18 +0200

    a

---
+++ Python/OverTheWire/natas28.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+from pkcs7 import PKCS7Encoder
+import pkcs7,threading, base64
+
+
+text='my secret data'
+key = 'secret#456!23key'
+iv = 'Key@123Key@123fd'
+aes = AES.new(key, AES.MODE_CBC, iv)
+encoder = PKCS7Encoder()
+pad_text = encoder.encode(text)
+cipher = aes.encrypt(pad_text)
+enc_cipher = base64.b64encode(cipher)
+print enc_cipher
